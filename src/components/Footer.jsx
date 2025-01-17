d919c7b3aed075f61a54171715962a057bc0547c
Ishwar Chaudhary <ishwarura@gmail.com>
2025-01-17 11:57:22 +0545

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -76,7 +76,7 @@ const Footer = () => {
               rel="noopener noreferrer"
               className="inline-block mt-4 py-2 px-4 bg-green-700 text-white rounded hover:bg-blue-500 transition-colors"
             >
-              Know more about Alternate Energy Promotion Center
+              Know more about Alternative Energy Promotion Center
             </a>
           </div>
 
